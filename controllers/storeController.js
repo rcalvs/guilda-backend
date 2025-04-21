2c0a8905012f3c8ea504d6e8bf0c9e9fc0ea6ea0
rcalvs <rafaelcalvette@gmail.com>
2025-04-20 22:40:50 -0400

    addinng store route

---
+++ controllers/storeController.js
@@ -0,0 +1,57 @@
+const redis = require('../config/redisClient');
+const { v4: uuidv4 } = require('uuid');
+
+exports.createStore = async (req, res) => {
+  try {
+    const id = uuidv4();
+    const store = { id, ...req.body };
+    await redis.set(`store:${id}`, JSON.stringify(store));
+    res.status(201).json(store);
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao criar loja.' });
+  }
+};
+
+exports.getAllStore = async (req, res) => {
+  try {
+    const keys = await redis.keys('store:*');
+    const values = await redis.mget(keys);
+    const stores = values.map(val => JSON.parse(val));
+    res.json(stores);
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao buscar lojas.' });
+  }
+};
+
+exports.getStoreById = async (req, res) => {
+  try {
+    const store = await redis.get(`store:${req.params.id}`);
+    if (!store) return res.status(404).json({ error: 'loja não encontrstoreo.' });
+    res.json(JSON.parse(store));
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao buscar loja.' });
+  }
+};
+
+exports.updateStore = async (req, res) => {
+  try {
+    const store = await redis.get(`store:${req.params.id}`);
+    if (!store) return res.status(404).json({ error: 'loja não encontrstoreo.' });
+
+    const updatedstore = { ...JSON.parse(store), ...req.body };
+    await redis.set(`store:${req.params.id}`, JSON.stringify(updatedstore));
+    res.json(updatedstore);
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao atualizar loja.' });
+  }
+};
+
+exports.deleteStore = async (req, res) => {
+  try {
+    const deleted = await redis.del(`store:${req.params.id}`);
+    if (!deleted) return res.status(404).json({ error: 'loja não encontrstoreo.' });
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao deletar loja.' });
+  }
+};
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const adRoutes = require("./routes/adRoutes");
+const storeRoutes = require("./routes/storeRoutes")
 const checkApiKey = require('./middleware/apiKey');
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(checkApiKey);
 
 app.use("/ads", adRoutes);
+app.use("/stores", storeRoutes);
 
 app.get("/", (req, res) => {
   res.send("API funcionando no Render!");
